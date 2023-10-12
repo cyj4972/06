b3297d8ab55a627e90f9f7ae374709ba944484c1
cyj4972 <144991384+cyj4972@users.noreply.github.com>
2023-10-12 16:25:52 +0900

    week6_4

--- main_week6_1.c
+++ main_week6_1.c
@@ -3,41 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
-	{
-	int result;
-	result= a + b;
-	return result;
-	}
-int square( int n)
-	{
-		return n*n;
-	}
-int get_max(int x, int y)
-	{
-		if(x > y)
-			return x;
-		else
-			return y;
-		//if( x > y)
-			//return x;
-			
-		//return y;
-	}
-int main(int argc, char *argv[]) {
-	int a, b;
-	int n;
-	int x, y;
+int square(int a)
+{
+	return(a*a);
+}
 
-	a = 3;
-	b = 10;
-	printf("sumTwo result : %i\n", sumTwo(a, b));
-	
-	n=2;
-	printf("square result : %i\n", square(n));
-	
-	x=3, y=2;
-	printf("get_max result : %i\n", get_max(x, y));
+int main(int argc, char *argv[]) {
+	int a = 2;
+	a = square(a);
+	printf("a=%i\n",a);
 	
 	system("PAUSE");
 	return 0;
